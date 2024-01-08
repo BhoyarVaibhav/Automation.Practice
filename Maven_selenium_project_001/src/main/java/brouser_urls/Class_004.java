f8a00deb6b892e33628e9e84ca7a9f2be2a39894
BhoyarVaibhav <155597529+BhoyarVaibhav@users.noreply.github.com>
2024-01-08 09:21:55 +0530

    added i java class

---
+++ Maven_selenium_project_001/src/main/java/brouser_urls/Class_004.java
@@ -0,0 +1,13 @@
+package brouser_urls;
+
+import base_class.Base_class;
+
+public class Class_004 extends Base_class {
+
+	public static void main(String[] args) {
+		
+		launch_brouser("Chrome");
+		
+	}
+
+}
